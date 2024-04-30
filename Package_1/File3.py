c668a9a1fcd9d7fc39f81685f356ac7d462067d4
takhangithub <takhan610@gmail.com>
2024-04-29 22:55:51 -0500

    first commit

---
+++ Package_1/File3.py
@@ -0,0 +1,31 @@
+#print(("alice"+" " )* 3)
+
+
+#name = input("enter your name")
+#print("my name : " + name)
+
+#myAge = input()
+# 1) printing myAge as int - concatenate using comma
+#print( 'your age  ' , int(myAge))
+
+# 2)  print all as string using plus sign
+#print( 'your age will be ' + str(int(myAge)+1) +"in a year")
+#print('your age will be ',int(myAge)+1, 'in a year')
+
+
+# Expression / programming is combination of values, variables, declared or built functions....
+# Expression returns / evaluate a single value
+# Common rules to create expression
+# Syntax Rules:
+# Evaluation Rules:  how a expression get evaluated, - expression get evaluated according to language,
+# as example
+# Type Rules:  :  variable and concatenation rules
+# Function Calls:  expression can call function, such as using print / input() which is built in function
+# Error handling : approach of error handling at the time of coding
+
+
+myName = 'taslim'
+verifyName = input()
+if myName == verifyName:
+    print('name ok')
+
